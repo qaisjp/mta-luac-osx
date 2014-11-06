8119522cd9428afe32f7b23a9b85c8a9f84bac63
Qais Patankar <qaisjp@gmail.com>
2014-11-06 19:09:03 +0000

    Add progress indicators and broken error handling

--- MTA Lua Compiler/MTAFileCompiler.swift
+++ MTA Lua Compiler/MTAFileCompiler.swift
@@ -12,10 +12,18 @@ import Alamofire
 class MTAFileCompiler: NSObject {
     let LUAC_MTASA_COM:NSURLRequest = NSURLRequest(URL: NSURL(string: "http://luac.mtasa.com/index.php")!)
     
-    init(file:NSURL, obfuscate:Bool = false, debug:Bool = false) {
+    enum CompleteReason {case CompileError, SomeOtherError, Success, None}
+    
+    var responseCallback: ((CompleteReason, String?)->Void)?
+    var request:Request?
+    var fileURL:NSURL?
+    
+    init(file:NSURL, obfuscate:Bool = false, debug:Bool = false, callback:((CompleteReason, String?)->Void)?) {
+        
         super.init()
     
-        println("Received " + file.absoluteString!)
+        self.fileURL = file
+        self.responseCallback = callback
         
         var parameters: [String: String] = [
             "compile": "1",
@@ -23,33 +31,37 @@ class MTAFileCompiler: NSObject {
             "debug": debug ? "1" : "0"
         ]
         
+        var mtaURLrequest = Alamofire.ParameterEncoding.URL.encode(LUAC_MTASA_COM, parameters: parameters).0
         
-        var test = Alamofire.ParameterEncoding.URL.encode(LUAC_MTASA_COM, parameters: parameters)
-        
-        var upload = Alamofire.upload(.POST, test.0, file)
-            .progress { (bytesWritten, totalBytesWritten, totalBytesExpectedToWrite) in
-            println("Total bytes written \(totalBytesWritten)")
-        }
-
-        .response { (request,response,d,error) in
-            if (error != nil) {
-                println(error)
-                return
+        self.request = Alamofire.upload(.POST, mtaURLrequest, file)
+            .response { (request,response,d,error) in
+                var callback:((CompleteReason, String?)->Void)? = self.responseCallback
+                
+                if (error != nil) {
+                    println(error)
+                    if (callback != nil) {
+                        println("Output")
+                        callback!(.CompileError, error?.description)
+                    }
+                    return
+                }
+                
+                var data:NSData = d as NSData
+                var str = NSString(data:data, encoding:NSUTF8StringEncoding)
+                
+                if (str != nil) {
+                    // Error! str is not null if plain text is returned
+                    println("Error: " + str!)
+                    return
+                }
+                
+                // Time to save to file
+                data.writeToURL(file.URLByAppendingPathExtension("luac"), atomically: true)
+                
+                if callback != nil {
+                    callback!(CompleteReason.Success, nil)
+                }
+                
             }
-            
-            var data:NSData = d as NSData
-            var str = NSString(data:data, encoding:NSUTF8StringEncoding)
-            
-            if (str != nil) {
-                // Error! str is not null if plain text is returned
-                println("Error: " + str!)
-                return
-            }
-            
-            // Time to save to file
-            println(data)
-            data.writeToURL(file.URLByAppendingPathExtension("luac"), atomically: true)
-            
-        }
     }
 }
--- MTA Lua Compiler/MTAMainViewController.swift
+++ MTA Lua Compiler/MTAMainViewController.swift
@@ -10,10 +10,16 @@ import Cocoa
 
 class MTAMainViewController: NSViewController {
     var parameters: Array<NSURL>?
+    var currentCompiler: MTAFileCompiler?
+    
+    @IBOutlet weak var fileManagerButton: NSButton!
+    @IBOutlet weak var compileBtn: NSButton!
+    @IBOutlet weak var obfuscateCheckbox: NSButton!
+    @IBOutlet weak var uploadProgress: NSProgressIndicator!
+    @IBOutlet weak var fileProgress: NSLevelIndicator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
     }
     
     override func prepareForSegue(segue: NSStoryboardSegue, sender: AnyObject?) {
@@ -22,17 +28,64 @@ class MTAMainViewController: NSViewController {
     }
     
     
-    @IBOutlet weak var obfuscateCheckbox: NSButton!
-    @IBAction func onCompilePress(sender: AnyObject) {
-        
-        if (parameters == nil) {
-            println("choose files first")
+    @IBOutlet weak var messageField: NSTextField!
+    func setMessage(message: NSString) {
+        messageField.stringValue = message
+        println("setMessage: " + message)
+    }
+
+    @IBAction func onButtonPress(sender: NSButton) {
+        if (currentCompiler != nil) {
+            // Do some cancel stuff
+            currentCompiler?.request?.cancel()
+            onCompileFinish(MTAFileCompiler.CompleteReason.None, error: "Compile cancelled")
             return
         }
         
+        if (parameters == nil) {
+            setMessage("No files have been selected")
+            return
+        }
+        
+        compileBtn.title = "Cancel"
+        fileManagerButton.enabled = false
+        
+        var numberFormatter = NSNumberFormatter()
+        numberFormatter.format = "0"
+        
         var shouldObfuscate: Bool = obfuscateCheckbox.state == NSOnState
 
-        var compiler: MTAFileCompiler = MTAFileCompiler(file: parameters![0], obfuscate: shouldObfuscate, debug: false)
+        currentCompiler = MTAFileCompiler(file: parameters![0], obfuscate: shouldObfuscate, debug: false, callback: onCompileFinish)
+        
+        var progressMessage: String = "Uploading \"\(currentCompiler!.fileURL!.lastPathComponent)\""
+        
+        currentCompiler!.request!.progress { (written, total, expected) in
+            var percent:Double = Double(total) / Double(expected) * 100
+            var message = progressMessage + " (\(numberFormatter.stringFromNumber(percent)!)%)"
+            self.setMessage(message)
+            self.uploadProgress.doubleValue = percent
+        }
+        
+    }
+    
+    func onCompileFinish(reason: MTAFileCompiler.CompleteReason, error: String?) {
+        compileBtn.title = "Compile files"
+        fileManagerButton.enabled = true
+        
+        switch reason {
+        case .Success:
+            self.setMessage("Compiled \"\(currentCompiler!.fileURL!.lastPathComponent)\"")
+        case .CompileError:
+            self.setMessage(error!)
+        case .SomeOtherError:
+            self.setMessage("Internal error. Please try again.")
+        case .None:
+            if error != nil {
+                self.setMessage(error!)
+            }
+        }
+        
+        currentCompiler = nil
     }
     
 }
